54d960c4137f30220d92f5cc1745ad56bf52265e
Hugi Thordarson <hugi@karlmenn.is>
2025-04-22 14:46:12 +0000

    Read the actual adaptor config from wotaskd

---
+++ src/main/java/modulo/AdaptorConfigParser.java
@@ -0,0 +1,140 @@
+package modulo;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ * Experimental parsing of SiteConfig.xml
+ *
+ * Needs a SiteConfig.xml file in src/main/resources to function (one is currently not provided by default)
+ */
+
+public class AdaptorConfigParser {
+
+	public record Instance( int id, String host, int port ) {}
+
+	public record Application( String name, List<Instance> instances ) {}
+
+	public record AdaptorConfig( List<Application> applications ) {
+
+		public Application application( String applicationName ) {
+			for( Application application : applications() ) {
+				if( application.name().equals( applicationName ) ) {
+					return application;
+				}
+			}
+
+			return null;
+		}
+	}
+
+	public static AdaptorConfig adaptorConfig() {
+		final AdaptorConfig config = new AdaptorConfig( new ArrayList<>() );
+		importApplications( config );
+		return config;
+	}
+
+	private static void importApplications( final AdaptorConfig config ) {
+		final Document siteConfig = readConfigDocument();
+
+		final NodeList nodes = siteConfig
+				.getElementsByTagName( "adaptor" ).item( 0 )
+				.getChildNodes();
+
+		for( int i = 0; i < nodes.getLength(); i++ ) {
+			final Node applicationNode = nodes.item( i );
+
+			if( applicationNode.getNodeType() == Node.ELEMENT_NODE ) {
+				final NodeList instanceNodes = applicationNode.getChildNodes();
+
+				// Items to populate for each node
+				String applicationName = applicationNode
+						.getAttributes()
+						.getNamedItem( "name" )
+						.getNodeValue();
+
+				final Application application = new Application( applicationName, new ArrayList<>() );
+				config.applications().add( application );
+
+				for( int j = 0; j < instanceNodes.getLength(); j++ ) {
+					final Node instanceNode = instanceNodes.item( j );
+
+					if( instanceNode.getNodeType() == Node.ELEMENT_NODE ) {
+						final NamedNodeMap attributes = instanceNode.getAttributes();
+
+						String id = attributes.getNamedItem( "id" ).getNodeValue();
+						String host = attributes.getNamedItem( "host" ).getNodeValue();
+						String port = attributes.getNamedItem( "port" ).getNodeValue();
+
+						application.instances().add( new Instance( Integer.valueOf( id ), host, Integer.valueOf( port ) ) );
+					}
+				}
+			}
+		}
+	}
+
+	private static Document readConfigDocument() {
+
+		// The password for wotaskd/JavaMonitor is provided as a system property
+		// FIXME: We should have a nicer way to provide this
+		final String wotaskdPassword = System.getProperty( "wotaskdpassword" );
+		final String wotaskdHost = "linode-4.rebbi.is"; // FIXME: Make configurable
+
+		final HttpRequest request = HttpRequest
+				.newBuilder( URI.create( "http://%s:1085/Apps/WebObjects/wotaskd.woa/wa/woconfig".formatted( wotaskdHost ) ) )
+				.headers( "password", wotaskdPassword )
+				.build();
+
+		byte[] bytes;
+
+		try {
+			bytes = HttpClient
+					.newHttpClient()
+					.send( request, BodyHandlers.ofByteArray() )
+					.body();
+		}
+		catch( IOException | InterruptedException e ) {
+			throw new RuntimeException( e );
+		}
+
+		final ByteArrayInputStream xmlStream = new ByteArrayInputStream( bytes );
+
+		//		try( final InputStream xmlStream = AdaptorConfigParser.class.getClassLoader().getResourceAsStream( "AdaptorConfig.xml" )) {
+		try {
+			final DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			final DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			final Document doc = dBuilder.parse( xmlStream );
+			// doc.getDocumentElement().normalize();
+			return doc;
+		}
+		catch( IOException | ParserConfigurationException | SAXException e ) {
+			throw new RuntimeException( e );
+		}
+	}
+
+	public static void main( String[] args ) {
+
+		for( Application application : adaptorConfig().applications() ) {
+			System.out.println( application.name() );
+			for( Instance instance : application.instances() ) {
+				System.out.println( " - " + instance );
+			}
+		}
+	}
+}
--- src/main/java/modulo/Modulo.java
+++ src/main/java/modulo/Modulo.java
@@ -56,15 +56,18 @@ public class Modulo {
 			super( httpURIRewriter );
 		}
 
+		// Header's we'll probably need to handle:
+		// x-webobjects-adaptor-version
+		// x-webobjects-request-id
+		// x-webobjects-request-method
+
+		// Example request headers from a request proxied by the Apache adaptor
+		// {Accept=[text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8], Accept-Encoding=[gzip, deflate, br, zstd], Accept-Language=[en-GB, en-US;q=0.7, en;q=0.3], connection=[close], DNT=[1], DOCUMENT_ROOT=[/rebbi/com.fermentedshark/html], Host=[www.fermentedshark.com], HTTPS=[on], mod_rewrite_rewritten=[1], Priority=[u=0, i], Referer=[https://www.fermentedshark.com/], REMOTE_ADDR=[31.209.136.250], REMOTE_HOST=[31.209.136.250], REMOTE_PORT=[49578], SCRIPT_FILENAME=[/Apps], SCRIPT_URI=[https://www.fermentedshark.com/page/templating], SCRIPT_URL=[/page/templating], Sec-Fetch-Dest=[document], Sec-Fetch-Mode=[navigate], Sec-Fetch-Site=[same-origin], Sec-Fetch-User=[?1], SERVER_ADMIN=[root@localhost], SERVER_NAME=[www.fermentedshark.com], SERVER_PORT=[443], SERVER_SOFTWARE=[Apache/2.4.6 (CentOS) OpenSSL/1.0.2k-fips], ssl-secure-reneg=[1], SSL_TLS_SNI=[www.fermentedshark.com], UNIQUE_ID=[aAdE0G3LH-oETpsNLF2qBwAAAAw], Upgrade-Insecure-Requests=[1], User-Agent=[Mozilla/5.0 (Macintosh;Intel Mac OS X 10.15;rv:139.0) Gecko/20100101 Firefox/139.0], x-webobjects-adaptor-version=[Apache], x-webobjects-request-id=[680605a00000534c00000a82], x-webobjects-request-method=[GET]}
 		@Override
 		protected void addProxyHeaders( Request clientToProxyRequest, org.eclipse.jetty.client.Request proxyToServerRequest ) {
 			super.addProxyHeaders( clientToProxyRequest, proxyToServerRequest );
 
 			logger.info( "Received headers: " + clientToProxyRequest.getHeaders() );
-
-			//			proxyToServerRequest.headers( headers -> headers.computeField( "some-header", ( header, viaFields ) -> {
-			//				return new HttpField( "some-header", "some-value" );
-			//			} ) );
 
 			proxyToServerRequest.headers( headers -> headers.computeField( "x-webobjects-adaptor-version", ( header, viaFields ) -> {
 				return new HttpField( "x-webobjects-adaptor-version", "Modulo" );
@@ -77,13 +80,6 @@ public class Modulo {
 			proxyToServerRequest.headers( headers -> headers.computeField( "x-webobjects-request-method", ( header, viaFields ) -> {
 				return new HttpField( "x-webobjects-request-method", clientToProxyRequest.getMethod() );
 			} ) );
-
-			// x-webobjects-adaptor-version
-			// x-webobjects-request-id
-			// x-webobjects-request-method
-
-			// Example request headers from a request proxied by the Apache adaptor
-			// {Accept=[text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8], Accept-Encoding=[gzip, deflate, br, zstd], Accept-Language=[en-GB, en-US;q=0.7, en;q=0.3], connection=[close], DNT=[1], DOCUMENT_ROOT=[/rebbi/com.fermentedshark/html], Host=[www.fermentedshark.com], HTTPS=[on], mod_rewrite_rewritten=[1], Priority=[u=0, i], Referer=[https://www.fermentedshark.com/], REMOTE_ADDR=[31.209.136.250], REMOTE_HOST=[31.209.136.250], REMOTE_PORT=[49578], SCRIPT_FILENAME=[/Apps], SCRIPT_URI=[https://www.fermentedshark.com/page/templating], SCRIPT_URL=[/page/templating], Sec-Fetch-Dest=[document], Sec-Fetch-Mode=[navigate], Sec-Fetch-Site=[same-origin], Sec-Fetch-User=[?1], SERVER_ADMIN=[root@localhost], SERVER_NAME=[www.fermentedshark.com], SERVER_PORT=[443], SERVER_SOFTWARE=[Apache/2.4.6 (CentOS) OpenSSL/1.0.2k-fips], ssl-secure-reneg=[1], SSL_TLS_SNI=[www.fermentedshark.com], UNIQUE_ID=[aAdE0G3LH-oETpsNLF2qBwAAAAw], Upgrade-Insecure-Requests=[1], User-Agent=[Mozilla/5.0 (Macintosh;Intel Mac OS X 10.15;rv:139.0) Gecko/20100101 Firefox/139.0], x-webobjects-adaptor-version=[Apache], x-webobjects-request-id=[680605a00000534c00000a82], x-webobjects-request-method=[GET]}
 		}
 	}
 
