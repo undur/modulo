b908a99efc186ad7db51c8956ba1432bae49fd0f
Hugi Thordarson <hugi@karlmenn.is>
2025-04-23 07:52:33 +0000

    List running applications/instances on modulo-runner's main page

--- modulo-runner/src/main/java/modulo/runner/MDStartPage.java
+++ modulo-runner/src/main/java/modulo/runner/MDStartPage.java
@@ -1,11 +1,26 @@
 package modulo.runner;
 
+import java.util.List;
+
+import modulo.AdaptorConfigParser;
 import ng.appserver.NGContext;
 import ng.appserver.templating.NGComponent;
 
 public class MDStartPage extends NGComponent {
 
+	public modulo.AdaptorConfigParser.Application currentApplication;
+	public modulo.AdaptorConfigParser.Instance currentInstance;
+
 	public MDStartPage( NGContext context ) {
 		super( context );
+	}
+
+	public List<modulo.AdaptorConfigParser.Application> applications() {
+		return AdaptorConfigParser
+				.adaptorConfig()
+				.applications()
+				.values()
+				.stream()
+				.toList();
 	}
 }
