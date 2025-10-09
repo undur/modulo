da2ee84ee3bd1d6e4aef8c281f2191b9c4eb6e6a
Hugi Thordarson <hugi@karlmenn.is>
2025-10-09 07:40:50 +0000

    Start refactoring/cleanup in adaptor configuration reload

--- modulo-core/src/main/java/modulo/Modulo.java
+++ modulo-core/src/main/java/modulo/Modulo.java
@@ -24,13 +24,13 @@ import modulo.woadaptorconfig.model.Instance;
  * A jetty-based reverse proxy
  *
  * FIXME: We're currently always targeting the first instance. Here's where a load balacing scheme might come in strong... // Hugi 2025-04-22
- * FIXME: Oh, and we're going to have to target the correct instance based on the request URL // Hugi 2025-04-22
+ * FIXME: currently only support one instance. Target instances based on the request URL and 'woinst' cookie // Hugi 2025-04-22
  * FIXME: We might want to gather some statistics and logging // Hugi 2025-04-22
- * FIXME: Adaptor config needs to be regularly updated/manually updatable // Hugi 2025-04-22
- * FIXME: We're missing configuration options for ... everything // Hugi 2025-04-22
  * FIXME: Add some nice error pages // Hugi 2025-04-22
  * FIXME: Target application based on domain // Hugi 2025-10-09
  * FIXME: Shutdown/startup of an application in a related wotaskd should really trigger an adaptor reload // Hugi 2025-10-09
+ * FIXME: Adaptor config needs to be manually updatable // Hugi 2025-04-22
+ * FIXME: We're missing user configuration for ... everything // Hugi 2025-04-22
  */
 
 public class Modulo {
@@ -39,25 +39,55 @@ public class Modulo {
 
 	/**
 	 * Duration between reloads of adaptor configuration
+	 *
+	 * FIXME: Should be settable/configurable // Hugi 2025-10-09
 	 */
 	private static final Duration DEFAULT_CONFIG_RELOAD_DURATION = Duration.ofSeconds( 10 );
 
 	/**
+	 * Adaptor URL
+	 *
+	 * FIXME: Should be settable/configurable // Hugi 2025-10-09
+	 */
+	private static final String ADAPTOR_URL = "/Apps/WebObjects/";
+
+	/**
 	 * The port to the proxy will run on
 	 */
-	private final int port;
+	private final int _port;
 
 	/**
 	 * FIXME: We're going to want to hold multiple adaptor configuration sources // Hugi 2025-05-02
 	 */
-	private AdaptorConfig adaptorConfig;
+	private AdaptorConfig _adaptorConfig;
 
 	/**
 	 * Construct a new instance running the proxy on the given port
 	 */
 	public Modulo( final int port ) {
-		this.port = port;
-		this.adaptorConfig = fetchAdaptorConfig();
+		_port = port;
+		_adaptorConfig = fetchAdaptorConfig();
+	}
+
+	/**
+	 * @return The host wotaskd is running on
+	 */
+	public static String wotaskdHost() {
+		return "hz1.rebbi.is";
+	}
+
+	/**
+	 * @return Port number to fetch wotaskd's configuration from
+	 */
+	public static int wotaskdPort() {
+		return 1085;
+	}
+
+	/**
+	 * @return The password for getting configuration from the targeted wotaskd instance
+	 */
+	public static String wotaskdPassword() {
+		return System.getProperty( "wotaskdpassword" );
 	}
 
 	public void start() {
@@ -66,7 +96,7 @@ public class Modulo {
 
 		final Server server = new Server();
 		final ServerConnector connector = new ServerConnector( server );
-		connector.setPort( port );
+		connector.setPort( _port );
 		server.addConnector( connector );
 		server.setHandler( new ModuloProxy( rewriteURIFunction() ) );
 
@@ -83,11 +113,11 @@ public class Modulo {
 	}
 
 	public void reloadAdaptorConfig() {
-		adaptorConfig = fetchAdaptorConfig();
+		_adaptorConfig = fetchAdaptorConfig();
 	}
 
 	public AdaptorConfig adaptorConfig() {
-		return adaptorConfig;
+		return _adaptorConfig;
 	}
 
 	private void startAdaptorConfigAutoReloader() {
@@ -106,18 +136,16 @@ public class Modulo {
 
 	/**
 	 * @return The adaptorConfig we'll initialize with.
-	 *
-	 * FIXME: This will be refactored out of existence soon // Hugi 2025-04-23
 	 */
 	private static AdaptorConfig fetchAdaptorConfig() {
 
 		AdaptorConfig config;
 
-		final String password = System.getProperty( "wotaskdpassword" );
+		final String password = wotaskdPassword();
 
 		if( password != null ) {
-			final int port = 1085;
-			final String host = "hz1.rebbi.is";
+			final int port = wotaskdPort();
+			final String host = wotaskdHost();
 			config = new AdaptorConfigParser( host, port, password ).fetchAdaptorConfig();
 		}
 		else {
@@ -143,7 +171,7 @@ public class Modulo {
 
 			final String applicationName = applicationNameFromURI( originalURI );
 
-			final App application = adaptorConfig.application( applicationName );
+			final App application = _adaptorConfig.application( applicationName );
 
 			if( application == null ) {
 				throw new IllegalArgumentException( "No application found with the name %s".formatted( applicationName ) );
@@ -177,24 +205,14 @@ public class Modulo {
 	 * @return The name of the application from the given URI
 	 */
 	static String applicationNameFromURI( final HttpURI uri ) {
-		final String[] splitPath = uri.getPath().split( "/" );
+		String appName = uri.getPath().substring( ADAPTOR_URL.length() );
 
-		if( splitPath.length < 4 ) {
-			throw new IllegalArgumentException( "URL too short" );
+		int periodIndex = appName.indexOf( ".woa" );
+
+		if( periodIndex > -1 ) {
+			appName = appName.substring( 0, periodIndex );
 		}
 
-		String applicationName = splitPath[3];
-
-		// Remove the .woa from the application name
-		applicationName = applicationName.split( "\\." )[0];
-
-		return applicationName;
-	}
-
-	/**
-	 * Main method included for quick testing
-	 */
-	public static void main( String[] argv ) {
-		new Modulo( 1400 ).start();
+		return appName;
 	}
 }
